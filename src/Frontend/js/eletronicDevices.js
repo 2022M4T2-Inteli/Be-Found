9f977fc3f6f7dcfeb023919eef2b8f123a6f6f52
João Alcaraz <joao.alcaraz123@gmail.com>
2022-11-23 15:59:48 -0300

    bug  fix

--- src/Frontend/js/eletronicDevices.js
+++ src/Frontend/js/eletronicDevices.js
@@ -44,7 +44,7 @@ const getDevices = () => {$.ajax({
 })
 };
 
-buttonBuzzer(){
+function buttonBuzzer(){
     var buzzer = {
         "url": "http://127.0.0.1:5500/wifi/teste",
         "method": "PATCH",
