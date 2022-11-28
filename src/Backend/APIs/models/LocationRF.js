b00761b43fae7f351c0189262aa5881ef74f6259
Bruno Meira <99202553+brun0meira@users.noreply.github.com>
2022-11-28 20:19:09 -0300

    Updates backend pag dispositivos

--- src/Backend/APIs/models/LocationRF.js
+++ src/Backend/APIs/models/LocationRF.js
@@ -1,16 +1,34 @@
 const mongoose = require('mongoose');
+var moment = require('moment');
+moment.locale("pt-br");
 
 const locationRf = mongoose.model('locationRf',{
     modelo:String,
     localizacao:String,
     rec:String,
-    data:{type: Date,
-        default: Date.now
+    timestamp:{type: String,
+        default: () => moment().format('L')
     },
     buzer:{
         type: Number,
         default: 0,
-    }, 
+    },
+    version:{
+        type: String,
+        default: "Indefinido",
+    },
+    beaconP:{
+        type: String,
+        default: "Indefinido",
+    },
+    idd:{
+        type: String,
+        default: "Indefinido",
+    },
+    idp:{
+        type: String,
+        default: "Indefinido",
+    },
 })
 
 module.exports = locationRf;
--- src/Frontend/js/eletronicDevices.js
+++ src/Frontend/js/eletronicDevices.js
@@ -5,6 +5,7 @@ $(document).ready(() => {
 const geralDevices = document.querySelector("#geralDevices");
 const lastLocation = document.querySelector("#lastLocation");
 const register = document.querySelector("#registerData");
+const addInputdiv = document.querySelector("#info");
 
 var aux = 0;
 var ndev = 0;
@@ -28,10 +29,9 @@ const getDevices = () => {$.ajax({
             <div class="devices">
                 <div class="textDevices">
                     <img style="width: 12px; height: 12px;"class="circleFillGreenList" src="../imagens/circleFillGreen.png" alt="">
-                    <p><b>${element.rec}</b><br>${element.data}</p>
+                    <p><b>${element.rec}</b><br>${element.timestamp}</p>
                 </div>
                 <div class="imgCategoryDevice">
-                    <img class="mapPinList" src="../imagens/map-pin.svg" alt="map_pin">
                     <section class="textCategory"><p id="qcaraio`+idFilter+`">${element.modelo}</p></section>
                 </div>
             </div>
@@ -47,41 +47,91 @@ const getDevices = () => {$.ajax({
 };
 
 
-
-// document.getElementById("inputInfoDevice1").value = "My value";
-
 function deviceDetail(id){
+    // Div para conter os botoes do buzzer e de opções
+    addInputdiv.innerHTML = "";
+    const divButtonsTop = document.createElement("div");
+    divButtonsTop.classList.add('topInfoDevice');
+    divButtonsTop.setAttribute("id", "topButtonsDevice");
+    addInputdiv.appendChild(divButtonsTop);
+    const topButtonsList = document.querySelector("#topButtonsDevice");
+
     const deced = document.querySelector("#currentLocDev");
     const decedd = document.querySelector("#lastLocDev");
     deced.innerHTML = "";
     decedd.innerHTML = "";
     register.innerHTML = "";
+    
     console.log("BUCETAAAAA");
     $.ajax({
         url: "http://127.0.0.1:5500/rfid/find",
         type: 'GET',
         success: data => {
             data.forEach(element => {
+                // Botão Buzzer definições de propriedades
+                const buttonBuzzer = document.createElement("button");
+                buttonBuzzer.classList.add('volButton');
+                buttonBuzzer.setAttribute("id", "btnMusic");
+                buttonBuzzer.setAttribute("onclick", `buttonBuzzer('${element._id}')`);
+                // Botão opções definições de propriedades
+                const moreButtonsTopDev = document.createElement("div");
+                moreButtonsTopDev.classList.add('dropdown');
+                // Inputs definições de propriedades
+                const inputDiv = document.createElement("div");
+                inputDiv.classList.add('inputsDevice');
+                inputDiv.setAttribute("id", "inputsDivs");
+                // Informações de registro
                 const tittleView = document.createElement("div");
                 tittleView.classList.add('registerr');
                 tittleView.innerHTML = `
-                <p>Registrado em ${element.data}</p>
+                <p>Registrado em ${element.timestamp}</p>
                 <div class="connectDevide">
                     <img class="circleFillGreenInfo" src="../imagens/circleFillGreen.png" alt="">
                     <p>Conectado</p>
+                </div>
+                `
+                buttonBuzzer.innerHTML = `
+                <img src="../imagens/volume-2.svg" alt="">
+                `
+                moreButtonsTopDev.innerHTML = `
+                <button id="btnMore" class="moreButton" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="../imagens/more-horizontal.svg" alt="">
+                </button>
+                <ul class="dropdown-menu">
+                    <li><button onclick="editInfos('${element._id}')" class="btn"><img src="../imagens/editInfos.png" width="30" height="30" alt=""><span class="editSpan">Editar</span></button></li>
+                    <li><button class="btn"><img src="../imagens/attInfos.png" width="30" height="30" alt=""><span class="attSpan">Atualizar</span></button></li>
+                    <li><button class="btn"><img src="../imagens/removeDevices.png" width="30" height="30" alt=""><span class="removSpan">Desconectar</span></button></li>
+                </ul>
+                `
+                inputDiv.innerHTML = `
+                <div class="oneInput">
+                    <label>Nome do Dispositivo</label>
+                    <input name="${element._id}" onfocusout="disableField(0,'${element._id}','${element.rec}')" class="inputInfoDevice" type="text" value="${element.rec}" placeholder="Nome do Dispositivo" disabled>
+                    <label>Versão</label>
+                    <input name="${element._id}" onfocusout="disableField(1,'${element._id}','${element.rec}')" class="inputInfoDevice" type="text" value="${element.version}" placeholder="Versão" disabled>
+                    <label>ID do Dispositivo</label>
+                    <input name="${element._id}" onfocusout="disableField(2,'${element._id}','${element.rec}')" class="inputInfoDevice" type="text" value="${element.idd}" placeholder="ID do Dispositivo" disabled>
+                    
+                </div>
+                <div class="twoInput">
+                    <label>Ultima atualização</label>
+                    <input name="${element._id}" onfocusout="disableField(3,'${element._id}','${element.rec}')" class="inputInfoDevice" type="text" value="${element.timestamp}" placeholder="Ultima atualização" disabled>
+                    <label>Beacon Patrimônio</label>
+                    <input name="${element._id}" onfocusout="disableField(4,'${element._id}','${element.rec}')" class="inputInfoDevice" type="text" value="${element.beaconP}" placeholder="Beacon Patrimônio" disabled>
+                    <label>ID do Produto</label>
+                    <input name="${element._id}" onfocusout="disableField(5,'${element._id}','${element.rec}')" class="inputInfoDevice" type="text" value="${element.idp}"placeholder="ID do Produto" disabled>
                 </div>
                 `
                 const currentLoc = `${element.localizacao}`;
                 const lastLoc = `${element.localizacao}`;
                 var idDevicess = `${element._id}`;
                 if(idDevicess == id){
-                    document.getElementById("btnMusic").disabled = false;
-                    document.getElementById("btnMore").disabled = false;
                     register.appendChild(tittleView);
                     $("#currentLocDev").append(currentLoc);
                     $("#lastLocDev").append(lastLoc);
-                    document.getElementById("input0").value = `${element.rec}`;
-                    document.getElementById("input3").value = `${element.data}`;
+                    topButtonsList.appendChild(buttonBuzzer);
+                    topButtonsList.appendChild(moreButtonsTopDev);
+                    addInputdiv.appendChild(inputDiv);
                 }
             });
         }
@@ -149,22 +199,35 @@ function cleanfilter(){
 
 }
 
-function editInfos(){
+function editInfos(id){
     for (i = 0; i <= 5; i++) {
-        document.getElementById('input' + i).disabled = false;
-        document.getElementById('input' + i).placeholder = "";
-        document.getElementById('input' + i).style.background = "white";
+        document.getElementsByName(id)[i].disabled = false;
+        document.getElementsByName(id)[i].placeholder = "";
+        document.getElementsByName(id)[i].style.background = "white";
     }
 }
 
-function disableField(number){
-    document.getElementById('input' + number).disabled = true;
-    document.getElementById('input' + number).style.background = "#F2F2F2";
+function disableField(n, id){
+    var change = ["rec", "version", "idd", "timestamp", "beaconP", "idp"];
+    var changeOn = change[n];
+    var valueInput = document.getElementsByName(id)[n].value;
+    var editInput = {
+        "url": `http://127.0.0.1:5500/wifi/${id}`,
+        "method": "PATCH",
+        "timeout": 0,
+        "data": {
+            changeOn: valueInput,
+        }
+      };
+      
+      $.ajax(editInput);
+    document.getElementsByName(id)[n].disabled = true;
+    document.getElementsByName(id)[n].style.background = "#F2F2F2";
 }
 
-function buttonBuzzer(){
+function buttonBuzzer(id){
     var buzzer = {
-        "url": "http://127.0.0.1:5500/wifi/teste",
+        "url": `http://127.0.0.1:5500/wifi/${id}`,
         "method": "PATCH",
         "timeout": 0,
         "data": {
