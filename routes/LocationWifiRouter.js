954ce1655a4db6742dcbf901f3a6021d06e9da08
Bruno Meira <99202553+brun0meira@users.noreply.github.com>
2022-12-12 19:27:11 -0300

    agr vai?

--- routes/LocationWifiRouter.js
+++ routes/LocationWifiRouter.js
@@ -214,5 +214,26 @@ router.delete('/del/:id', async (req, res) => {
     }
 })
 
+router.patch('/timestamp/:id', async (req, res) => {
+    const id = req.params.id;
+    const timestamp = moment().startOf('hour').fromNow();
+    
+    const buz = { 
+        timestamp:timestamp, 
+    };
 
+    // const buz = buzer;
+    try {
+        const updateLoc = await locationWifi.updateOne({ _id: id }, buz);
+        if (updateLoc.matchedCount === 0) {
+            res.status(424).json({ msg: "Não encontrado" });
+            return
+        }
+        res.status(200).json(buz);
+
+    }
+    catch (error) {
+        res.status(500).json({ error: error });
+    }
+})
 module.exports = router;
